c70814c553060efc9195bbca54101da2e3075bd8
eduard-balamatiuc <balamatiuc2@gmail.com>
2025-03-31 16:14:06 +0300

    docs: update documentation

--- laboratory_3_2/src/globals.cpp
+++ laboratory_3_2/src/globals.cpp
@@ -1,7 +1,9 @@
 // globals.cpp
 #include "globals.h"
 
-// Define the variables here (only once)
+// Define the LCD dimensions
 int lcdColumns = 16;
 int lcdRows = 2;
+
+// Initialize the LCD object with I2C address 0x27
 LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
